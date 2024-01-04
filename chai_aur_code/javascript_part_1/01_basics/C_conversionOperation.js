faa7f4d9236a456ecd68348e445a25e3ff118087
rushikesh <rushithorst1234@gmail.com>
2024-01-04 18:32:04 +0530

    changes

--- chai_aur_code/javascript_part_1/01_basics/C_conversionOperation.js
+++ chai_aur_code/javascript_part_1/01_basics/C_conversionOperation.js
@@ -1,7 +1,28 @@
-let score = "10"
-console.log(typeof score)
-console.log(typeof(score))
+// let score = "10"
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valInNum = Number(score)
-console.log(typeof valInNum)
-console.log(valInNum)
+// let valInNum = Number(score)
+// console.log(typeof valInNum)
+// console.log(valInNum)
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let srt1 = "hello "
+// let str2 = "rushi"
+// let str3 = srt1+str2 
+// console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2 +2);
+console.log(1+2 +"2");
