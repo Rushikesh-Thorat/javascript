c229c8ce381bc43193cdc147dff0cdc55dd9eeff
rushikesh <rushithorst1234@gmail.com>
2024-01-04 12:40:20 +0530

    datatypes

---
+++ chai_aur_code/javascript_part_1/01_basics/A_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345 //constant not change 
+let accountEmail = "rushi@gmail.com"
+var accountPassword = "12345"
+accountCity = "pune"
+
+// accountId = 2 //not allowed
+accountEmail ="rushi2@gmail.com"
+accountPassword ="121212"
+accountCity ="narayangaon"
+console.log(accountId);
+
+console.table([accountEmail,accountId ,accountPassword,accountCity])
+
---
+++ chai_aur_code/javascript_part_1/01_basics/B_datatypes.js
@@ -0,0 +1,9 @@
+"use Strict";
+// alert("Hello")
+// alert(3+3) // useing nodejs,not browser
+
+let name ="rushi"
+let age = 20
+let isLoggedIn = false
+
+console.log(typeof null);
