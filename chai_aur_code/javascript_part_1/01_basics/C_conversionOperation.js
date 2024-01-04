9d47ba88370649de39e8b778657c2e53d7823f6f
rushikesh <rushithorst1234@gmail.com>
2024-01-04 21:19:34 +0530

    summary

--- chai_aur_code/javascript_part_1/01_basics/C_conversionOperation.js
+++ chai_aur_code/javascript_part_1/01_basics/C_conversionOperation.js
@@ -6,8 +6,8 @@
 // console.log(typeof valInNum)
 // console.log(valInNum)
 
-let value =3
-let negValue = -value
+let value =3 
+let negValue = -value 
 // console.log(negValue);
 
 // console.log(2+2);
@@ -22,7 +22,22 @@ let negValue = -value
 // let str3 = srt1+str2 
 // console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2");
-console.log("1"+2 +2);
-console.log(1+2 +"2");
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2 +2);
+// console.log(1+2 +"2"); 
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1=num2=num3=2+2
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+
+
---
+++ chai_aur_code/javascript_part_1/01_basics/E_Datatypes_summary.js
@@ -0,0 +1,7 @@
+// Primitive datatypes
+// 7 Types : String , number , boolean , null , undefined , symbol , BigInt
+
+
+
+// Reference(Non primitive)
+// Array , Objects , functions
